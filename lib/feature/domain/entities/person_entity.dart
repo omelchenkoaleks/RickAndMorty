9952c5ce8e4d207558f126b1c085104585824243
omelchenkoaleks <omelchenkoaleks@gmail.com>
2022-01-27 12:26:27 +0200

    implementation PersonEntity

--- lib/feature/domain/entities/person_entity.dart
+++ lib/feature/domain/entities/person_entity.dart
@@ -1,1 +1,51 @@
-class PesonEntity {}
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+
+class PesonEntity extends Equatable {
+  final int id;
+  final String name;
+  final String status;
+  final String species;
+  final String type;
+  final String gender;
+  final LocatonEntity origin;
+  final LocatonEntity location;
+  final String image;
+  final List<String> episode;
+  final DateTime created;
+
+  const PesonEntity(
+      {@required this.id,
+      @required this.name,
+      @required this.status,
+      @required this.species,
+      @required this.type,
+      @required this.gender,
+      @required this.origin,
+      @required this.location,
+      @required this.image,
+      @required this.episode,
+      @required this.created});
+
+  @override
+  List<Object> get props => [
+        id,
+        name,
+        status,
+        species,
+        type,
+        gender,
+        origin,
+        location,
+        image,
+        episode,
+        created
+      ];
+}
+
+class LocatonEntity {
+  final String name;
+  final String url;
+
+  const LocatonEntity({this.name, this.url});
+}
