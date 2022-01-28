903614b87eff2938dc84742f1ab88ceb239005a6
omelchenkoaleks <omelchenkoaleks@gmail.com>
2022-01-28 08:42:41 +0200

    implemented usecases

---
+++ lib/feature/domain/usecases/search_person.dart
@@ -0,0 +1,27 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:rick_and_morty/core/error/failure.dart';
+import 'package:rick_and_morty/core/usecases/usecase.dart';
+import 'package:rick_and_morty/feature/domain/entities/person_entity.dart';
+import 'package:rick_and_morty/feature/domain/repositories/person_repository.dart';
+
+class SearchPerson extends UseCase<List<PesonEntity>, SearchPersonParams> {
+  final PersonRepository personRepository;
+
+  SearchPerson(this.personRepository);
+
+  Future<Either<Failure, List<PesonEntity>>> call(
+      SearchPersonParams params) async {
+    return await personRepository.searchPerson(params.query);
+  }
+}
+
+class SearchPersonParams extends Equatable {
+  final String query;
+
+  const SearchPersonParams({@required this.query});
+
+  @override
+  List<Object> get props => [query];
+}
